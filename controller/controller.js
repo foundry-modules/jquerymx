12fbadf6c29da9037e80d1751852833ee497bb4a
Jason Ramos <jason@stackideas.com>
2012-02-22 16:55:11 +0800

    Added defaultOptions as an alternative to defaults.

--- controller/controller.js
+++ controller/controller.js
@@ -738,7 +738,9 @@ steal('jquery/class', 'jquery/lang/string', 'jquery/event/destroyed', function($
 			 * [jQuery.Controller.prototype.update update];
 			 *
 			 */
-			this.options = extend( extend(true, {}, cls.defaults), options);
+			// !-- FOUNDRY HACK --! //
+			// Added defaultOptions as an alternative to defaults
+			this.options = extend( extend(true, {}, cls.defaults, cls.defaultOptions), options);
 
 			// !-- FOUNDRY HACK --! //
 			// Augment selector properties into selector functions.
